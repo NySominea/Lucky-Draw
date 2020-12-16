fc069cf3d7becef47d903dace4192805906b7fbc
NySominea <sominea.ny77@gmail.com>
2020-12-16 17:26:55 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Phone;
+use App\Models\Prize;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('dashboard.index');
+        return view('dashboard.index',[
+            'phones_count' => Phone::active()->count(),
+            'prizes_count' => Prize::active()->count()
+        ]);
     }
 }
